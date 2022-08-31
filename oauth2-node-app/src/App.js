e6a0bd49f1781f51af305f7f67afd0e8ca312d9d
derek williams <derek61@gmail.com>
2022-08-31 11:26:16 -0600

    remove dependency on bootstrap

--- oauth2-node-app/src/App.js
+++ oauth2-node-app/src/App.js
@@ -1,6 +1,3 @@
-import Button from "react-bootstrap/Button";
-import CardDeck from "react-bootstrap/CardDeck";
-import Card from "react-bootstrap/Card";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
@@ -32,49 +29,27 @@ function App() {
   return (
     <div className="App text-center container-fluid">
       {!loggedIn ? (
-        <>
+        <div>
           <img
             alt="simple"
-            className="mb-4"
             src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
             width="150"
           ></img>
-          <h1 className="h3 mb-3 font-weight-normal">Sign in with GitHub</h1>
-          <Button
-            type="primary"
-            className="btn"
-            size="lg"
-            href="https://github.com/login/oauth/authorize?client_id=97f7c4390006237e1758&redirect_uri=http://localhost:8080/oauth/redirect"
-          >
-            Sign in
-          </Button>
-        </>
+          <h1>Sign in with GitHub</h1>
+          <a href="https://github.com/login/oauth/authorize?client_id=97f7c4390006237e1758&redirect_uri=http://localhost:8080/oauth/redirect">Sign In</a>
+        </div>
       ) : (
-        <>
+        <div>
           <h1>Welcome!</h1>
           <p>
             This is a simple integration between OAuth2 on GitHub with Node.js
           </p>
 
-          <CardDeck>
-            {[...Array(3)].map((e, i) => (
-              <Card style={{ maxWidth: "25%", margin: "auto" }}>
-                <Card.Img variant="top" src={user.avatar_url} />
-                <Card.Body>
-                  <Card.Title>{user.name}</Card.Title>
-                  <Card.Text>{user.bio}</Card.Text>
-                  <Button
-                    variant="primary"
-                    target="_blank"
-                    href={user.html_url}
-                  >
-                    GitHub Profile
-                  </Button>
-                </Card.Body>
-              </Card>
-            ))}
-          </CardDeck>
-        </>
+          <img alt="user" src={user.avatar_url}></img>
+          <div>{user.name}</div>
+          <div>{user.bio}</div>
+          <a href={user.html_url}>GitHub Profile</a>
+        </div>
       )}
     </div>
   );
